f8db59f6fd5c8d705d0a613eb32dad961e9a8f1a
daniel-sfranco <daniel.sfranco346@outlook.com>
2026-07-06 22:20:45 -0300

    Adding data into the file

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import os
+import pandas as pd
+
+def get_data():
+    path = os.path.dirname(os.path.abspath(__file__))
+    train_df = pd.read_csv(os.path.join(path, 'train.csv'))
+    test_df = pd.read_csv(os.path.join(path, 'test.csv'))
+    return train_df, test_df
+
+
 def main():
-    print("Hello World!")
+    train_df, test_df = get_data()
+    print(train_df.info())
+    print(test_df.info())
 
 if __name__ == "__main__":
     main()
